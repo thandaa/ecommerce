b58fbf3cff002fd80da422889b5fb71ef4de82d9
DESKTOP-0OO649U\Admin <thandarayetopcom@gmail.com>
2024-03-24 22:59:18 +0630

    Creact frontend

---
+++ fronted/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;

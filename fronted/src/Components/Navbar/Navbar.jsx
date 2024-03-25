348fb04d1c24cc4341de110dda2a1e6b9e28bae0
thandaa <thandarayetopcom@gmail.com>
2024-03-25 23:22:31 +0630

    Include modified and untracked files

--- fronted/src/Components/Navbar/Navbar.jsx
+++ fronted/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png'; // Import the cart_icon
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
 
@@ -14,14 +16,14 @@ const Navbar = () => {
                 <p>Shopper</p>
             </div>
             <ul className="nav-menu">
-                <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("mens")}}><Link to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("womens")}}><Link to='womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("kids")}}><Link to='kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
-                <button>Login</button>
-                <img src={cart_icon} alt="Cart Icon" /> 
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='cart'><img src={cart_icon} alt="Cart Icon" /> </Link>
                 <div className="nav-cart-count">0</div>
             </div>
         </div>

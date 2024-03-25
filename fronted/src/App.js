65e4261726e89d1514f4426526e12d8314b44b87
thandaa <thandarayetopcom@gmail.com>
2024-03-25 23:14:43 +0630

    edit frontend

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,13 +1,36 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Shop from './Pages/Shop'
+import ShopCategory from './Pages/ShopCategory'
+import Product from './Pages/Product'
+import LoginSignup from './Pages/LoginSignup'
+import Cart from './Pages/Cart'
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
-      <Hero/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids"/>}/>
+        <Route path='/product' element={<Product/>}>
+            <Route path=':productID' element={<Product/>} />
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
